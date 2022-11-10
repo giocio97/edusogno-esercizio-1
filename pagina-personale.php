d827ac8b8e84d4559e0c3d7462b352a7f694f1d9
Giovanni Ciotta <giociotta@gmail.com>
2022-11-10 18:52:45 +0100

    eventi-user

--- pagina-personale.php
+++ pagina-personale.php
@@ -1,9 +1,11 @@
 <?php
+include('./php/config.php');
 session_start();
 if(!isset($_SESSION['loggato']) || $_SESSION['loggato']!== true){
     header("location: login.html");
     exit;
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -20,8 +22,29 @@ if(!isset($_SESSION['loggato']) || $_SESSION['loggato']!== true){
     <?php include __DIR__ . '/php/header.php'; ?>
     <main>
         <h2>Ciao  <?= $_SESSION["email"];?>  ecco i tuoi eventi </h2> 
+
+        <?php 
+            $emailUser = $_SESSION["email"];
+            $sql_eventi = $sql = "SELECT * FROM `eventi` WHERE `attendees` LIKE \'%$emailUser%\';";
+            if ($result = $mysqli->query($sql_eventi)){
+                if ($result->num_rows > 0){
+                    while($row = $result->fetch_array()){
+                        echo'
+                        <div>
+                        <h4>' . $row['nome_evento']. '</h4>
+                        <p>'. $row['data_evento'].'</p>
+                        </div>
+                        ';
+                    }
+                } 
+            }
+            // else{
+            //     echo '<div>non ci sono eventi</div>';
+            // }
+            $mysqli->close();
+        ?>
     </main>
  
-   
+
 </body>
 </html>
